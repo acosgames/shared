23d8e04e11c51549274d7394f75e96e6621e75fa
Joel <joelruiz2@gmail.com>
2021-12-31 02:01:41 -0600

    fixing ecosystem production

--- util/credentials.js
+++ util/credentials.js
@@ -2,9 +2,12 @@
 const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = () => {
+    console.log("NODE_ENV: ", NODE_ENV);
+
     if (NODE_ENV == 'prod' || NODE_ENV == 'production') {
+        console.log("LOADING PRODUCTION CREDENTIALS");
         return require('../credential/production.json');
     }
-
+    console.log("LOADING LOCALHOST CREDENTIALS");
     return require('../credential/localhost.json');
 }
