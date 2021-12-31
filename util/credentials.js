231ce05c126189f84ee3a965b48a94c0bb20faf3
Joel <joelruiz2@gmail.com>
2021-12-31 01:56:10 -0600

    fixing ecosystem production

--- util/credentials.js
+++ util/credentials.js
@@ -1,8 +1,8 @@
 
-const ACOSENV = process.env.ACOSENV;
+const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = () => {
-    if (ACOSENV == 'prod' || ACOSENV == 'production') {
+    if (NODE_ENV == 'prod' || NODE_ENV == 'production') {
         return require('../credential/production.json');
     }
 
